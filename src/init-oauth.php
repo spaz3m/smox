2c75d6ba90649635d22df1844c344e543f76a033
spaz3m <127884793+spaz3m@users.noreply.github.com>
2023-06-26 04:15:22 -0400

    Update init-oauth.php

--- src/init-oauth.php
+++ src/init-oauth.php
@@ -1,6 +1,6 @@
 <?php
 
-$discord_url = "{REDIRECT_URL_LINK_FROM_DASHBOARD}";
+$discord_url = "https://discord.com/api/oauth2/authorize?client_id=934506964371800084&redirect_uri=https%3A%2F%2Fsmox.lol&response_type=code&scope=identify%20guilds";
 header("Location: $discord_url");
 exit();
 
