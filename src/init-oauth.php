c6778efccc4698f64b2e334ec822bb7a70eece70
spaz3m <127884793+spaz3m@users.noreply.github.com>
2023-06-26 04:27:09 -0400

    Delete src directory

--- src/init-oauth.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$discord_url = "https://discord.com/api/oauth2/authorize?client_id=934506964371800084&redirect_uri=https%3A%2F%2Fsmox.lol&response_type=code&scope=identify%20guilds";
-header("Location: $discord_url");
-exit();
-
-?>
